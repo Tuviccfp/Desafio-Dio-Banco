3b722062b29ce327f4206050a0d07e34f50d31c2
Tuviccfp <victorcordeirofp@gmail.com>
2024-06-24 14:52:48 -0300

    criação da classe Banco

---
+++ src/main/java/org/example/entities/Banco.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Banco {
+    private String nome;
+    private List<Conta> listaContas;
+}
